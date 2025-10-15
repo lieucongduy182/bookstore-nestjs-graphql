99d54f0087b21f26ed9076a2d5d3200594179f57
lieucongduy182 <lieucongduy182@gmail.com>
2025-10-15 13:44:23 +0700

    fix the correction GqlAuthGuard

--- src/auth/guards/gql-auth.guard.ts
+++ src/auth/guards/gql-auth.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
-export class GplAuthGuard extends AuthGuard('jwt') {
+export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
